7a8d89877d7996b95618ca8c98dd097262c62207
Pratham <diamond123erock@gmail.com>
2026-02-02 17:27:16 +0530

    Delete backend directory

--- backend/src/routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { findRoute } = require('../controllers/routeController');
-const { validateRouteRequest } = require('../middleware/validator');
-
-router.post('/route', validateRouteRequest, findRoute);
-
-module.exports = router;
